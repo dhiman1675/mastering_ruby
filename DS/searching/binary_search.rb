c50f655a26027cd2b7aa526d04b18e550ed28a6a
ROHIT <dhiman1675@gmail.com>
2025-01-17 15:58:35 +0530

    binary + ternary using recursion

--- DS/searching/binary_search.rb
+++ DS/searching/binary_search.rb
@@ -19,6 +19,25 @@ def binary_search(arr, target)
   -1 # Target not found
 end
 
+def recursive_binary(arr, target)
+  left = 0
+  right = arr.length - 1
+  while left <= right
+    mid = (left + right) / 2
+    if arr[mid] == target
+      return mid
+    elsif arr[mid] < target
+      # left = mid + 1
+      recursive_binary(arr[mid + 1..right], target)
+    else
+      # right = mid - 1
+      recursive_binary(arr[left..mid - 1], target)
+    end
+  end
+
+  -1
+end
+
 # Example Usage
 arr = [1, 3, 5, 7, 9]
 puts binary_search(arr, 5) # Output: 2
@@ -27,3 +46,7 @@ puts binary_search(arr, 8) # Output: -1
 # Binary search is searching algorithm which works in sorted arrays.
 # Fist separates the array from mid, if the target value is less than the middle element,
 # the search continues in the left half. Otherwise, it searches in the right half
+
+# Binary search using Recursion
+puts binary_search(arr, 222)
+puts binary_search(arr, 9)
--- DS/searching/ternary_search.rb
+++ DS/searching/ternary_search.rb
@@ -24,7 +24,34 @@ def ternary_search(arr, target)
   -1
 end
 
-input = [1, 2, 5, 5, 9, 33, 44, 53, 84]
-input2 = [2, 3, 44, 67, 78, 89, 95, 103, 763, 999]
-puts ternary_search(input, 53)
-puts ternary_search(input2, 1113)
+def ternary_search_recursive(arr, left, right, target)
+  return -1 if left > right
+
+  partition_size = (right - left) / 3
+  mid1 = left + partition_size
+  mid2 = right - partition_size
+
+  # Check if target is at any of the midpoints
+  if arr[mid1] == target
+    mid1
+  elsif arr[mid2] == target
+    mid2
+  # Decide which third to search
+  elsif target < arr[mid1]
+    ternary_search_recursive(arr, left, mid1 - 1, target)
+  elsif target > arr[mid2]
+    ternary_search_recursive(arr, mid2 + 1, right, target)
+  else
+    # If target is between mid1 and mid2, narrow down to this segment
+    ternary_search_recursive(arr, mid1 + 1, mid2 - 1, target)
+  end
+end
+
+sorted_array = [1, 2, 5, 5, 9, 33, 44, 53, 84]
+sorted_array2 = [2, 3, 44, 67, 78, 89, 95, 103, 763, 999]
+sorted_array3 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+puts ternary_search(sorted_array, 53)
+puts ternary_search(sorted_array2, 1113)
+puts 'RECURSIVE:'
+puts ternary_search_recursive(sorted_array3, 0, sorted_array3.length - 1, 7)
+puts ternary_search_recursive(sorted_array, 0, sorted_array.length - 1, 53)
